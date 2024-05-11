1716979bce3850e1c9645210c30a151016c73271
Lex <vasickin@mail.ru>
2024-05-11 15:22:04 +0300

    Инструкция: if

--- java/help/Help.java
+++ java/help/Help.java
@@ -1,8 +1,32 @@
 package help;
 
 public class Help {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+        throws java.io.IOException{
+
+        char choice;
+
+        System.out.println("Справка:");
+        System.out.println(" 1. if");
+        System.out.println(" 2. switch");
+        System.out.print("Выбирите: ");
+        choice = (char) System.in.read();
+
+        System.err.println("\n");
+
+        switch (choice) {
+            case '1':
+                System.err.println("Инструкция if:\n");
+                System.err.println("if(условие) {");
+                System.err.println("  инструкция;");
+                System.err.println("}");
+                System.err.println("else {");
+                System.err.println("  инструкция;");
+                System.err.println("}");
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
 }
 
